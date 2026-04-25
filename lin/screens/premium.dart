309f064af7d466aaf68d817bf85a81e80d1a3113
Benard083 <161836835+Benard083@users.noreply.github.com>
2026-04-25 15:41:33 +0300

    Create premium.dart

---
+++ lin/screens/premium.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PremiumScreen extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Go Premium")),
+      body: Center(
+        child: Text("Unlock unlimited AI access"),
+      ),
+    );
+  }
+}
