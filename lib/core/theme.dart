81b54294468406a36326672274bd36a2023b1ba4
Benard083 <161836835+Benard083@users.noreply.github.com>
2026-04-25 15:31:37 +0300

    Create theme.dart

---
+++ lib/core/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Color(0xFF0F172A),
+    primaryColor: Color(0xFF6366F1),
+    useMaterial3: true,
+  );
+}
